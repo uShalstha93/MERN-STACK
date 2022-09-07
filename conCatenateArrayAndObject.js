15a89c9aa637725964ef1c6a5e611ddbe37937e3
Ushal <ushalstha93@gmail.com>
2022-09-07 08:49:56 +0545

    updated files

--- conCatenateArrayAndObject.js
+++ conCatenateArrayAndObject.js
@@ -1,4 +1,4 @@
-debugger;
+// debugger;
 
 const firstName = ['Ram','Shyam','Hari','Laxman','Rakesh'];
 const lastName = {
--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-console.log('This is Main Branch!!');  
+// const testVar = 2000;
+const outerFunction = () => {
+  const outerFuncVar = 5000;
+  const innerFunction = () => {
+    // debugger;
+    console.log(outerFuncVar);
+  }
+  return innerFunction;
+}
+var z = outerFunction;
+console.log(z)
